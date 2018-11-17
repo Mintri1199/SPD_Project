d3e42f0b00f350df57473c0daebdf2376d51c71f
Jackson Ho <mintri1199@gmail.com>
2018-11-16 18:59:12 -0800

    fix logic

--- Tofu/AppDelegate.swift
+++ Tofu/AppDelegate.swift
@@ -17,12 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let mainView: ViewController = ViewController()
+        window = UIWindow(frame: UIScreen.main.bounds)
         
+        let mainView: ViewController = ViewController()
         navigationController = UINavigationController(rootViewController: mainView)
         navigationController?.hero.isEnabled = true
-        if let window = self.window{
+        
+        
+        if let window = window {
             window.rootViewController = navigationController
+            window.makeKeyAndVisible()
         }
         return true
     }
